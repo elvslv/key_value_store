4ac1e56cd8982e65146929434c8889a5e5c2d0e2
Lena Vasileva <lenav@dropbox.com>
2018-05-27 20:44:39 -0700

    add tests

--- tests/key_value_store/MessageTests.cpp
+++ tests/key_value_store/MessageTests.cpp
@@ -1,9 +1,33 @@
 #include "key_value_store/CreateRequestMessage.h"
+#include "key_value_store/DeleteRequestMessage.h"
+#include "key_value_store/ReadRequestMessage.h"
 #include "gtest/gtest.h"
 
 namespace
 {
-TEST(MessagesTests, CreateRequestMessage)
+class KeyValueStoreMessagesTests : public testing::Test
+{
+public:
+    template <typename T>
+    void TestMessage(const T& message, const network::Address& sourceAddress, const network::Address& destAddress, const std::string& key, key_value_store::Message::MsgTypes expectedType, std::unique_ptr<T>& parsedCastedMessage)
+    {
+        ASSERT_EQ(message.getSourceAddress(), sourceAddress);
+        ASSERT_EQ(message.getDestinationAddress(), destAddress);
+        ASSERT_EQ(message.getKey(), key);
+
+        auto networkMessage = message.serialize();
+        auto parsedMessage = key_value_store::Message::parseMessage(networkMessage);
+        ASSERT_EQ(parsedMessage->getMessageType(), expectedType);
+
+        parsedCastedMessage.reset(dynamic_cast<T*>(parsedMessage.release()));
+        ASSERT_EQ(parsedCastedMessage->getSourceAddress(), sourceAddress);
+        ASSERT_EQ(parsedCastedMessage->getDestinationAddress(), destAddress);
+        ASSERT_EQ(parsedCastedMessage->getKey(), key);
+        ASSERT_EQ(parsedCastedMessage->getId(), message.getId());
+    }
+};
+
+TEST_F(KeyValueStoreMessagesTests, CreateRequestMessage)
 {
     network::Address sourceAddress("1.0.0.0:100");
     network::Address destAddress("2.0.0.0:200");
@@ -11,20 +35,33 @@ TEST(MessagesTests, CreateRequestMessage)
     std::string value = "value";
 
     key_value_store::CreateRequestMessage createRequestMessage(sourceAddress, destAddress, key, value);
-    ASSERT_EQ(createRequestMessage.getSourceAddress(), sourceAddress);
-    ASSERT_EQ(createRequestMessage.getDestinationAddress(), destAddress);
-    ASSERT_EQ(createRequestMessage.getKey(), key);
     ASSERT_EQ(createRequestMessage.getValue(), value);
 
-    auto networkMessage = createRequestMessage.serialize();
-    auto parsedMessage = key_value_store::Message::parseMessage(networkMessage);
-    ASSERT_EQ(parsedMessage->getMessageType(), key_value_store::Message::CREATE_REQUEST);
+    std::unique_ptr<key_value_store::CreateRequestMessage> parsedCastedMessage;
+    TestMessage<>(createRequestMessage, sourceAddress, destAddress, key, key_value_store::Message::CREATE_REQUEST, parsedCastedMessage);
+    ASSERT_EQ(parsedCastedMessage->getValue(), value);
+}
 
-    std::unique_ptr<key_value_store::CreateRequestMessage> parsedCreateRequestMessage(dynamic_cast<key_value_store::CreateRequestMessage*>(parsedMessage.release()));
-    ASSERT_EQ(parsedCreateRequestMessage->getSourceAddress(), sourceAddress);
-    ASSERT_EQ(parsedCreateRequestMessage->getDestinationAddress(), destAddress);
-    ASSERT_EQ(parsedCreateRequestMessage->getKey(), key);
-    ASSERT_EQ(parsedCreateRequestMessage->getValue(), value);
-    ASSERT_EQ(parsedCreateRequestMessage->getId(), createRequestMessage.getId());
+TEST_F(KeyValueStoreMessagesTests, DeleteRequestMessage)
+{
+    network::Address sourceAddress("1.0.0.0:100");
+    network::Address destAddress("2.0.0.0:200");
+    std::string key = "key";
+
+    key_value_store::DeleteRequestMessage deleteRequestMessage(sourceAddress, destAddress, key);
+
+    std::unique_ptr<key_value_store::DeleteRequestMessage> parsedCastedMessage;
+    TestMessage<>(deleteRequestMessage, sourceAddress, destAddress, key, key_value_store::Message::DELETE_REQUEST, parsedCastedMessage);
+}
+
+TEST_F(KeyValueStoreMessagesTests, ReadRequestMessage)
+{
+    network::Address sourceAddress("1.0.0.0:100");
+    network::Address destAddress("2.0.0.0:200");
+    std::string key = "key";
+
+    key_value_store::ReadRequestMessage readRequestMessage(sourceAddress, destAddress, key);
+    std::unique_ptr<key_value_store::ReadRequestMessage> parsedCastedMessage;
+    TestMessage<>(readRequestMessage, sourceAddress, destAddress, key, key_value_store::Message::READ_REQUEST, parsedCastedMessage);
 }
 }
